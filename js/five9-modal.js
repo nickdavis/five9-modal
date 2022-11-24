d9a2d640f285109b9cbba263b876ea9848834323
Nick Davis <inbox@nickdavis.net>
2022-11-24 12:35:18 +0000

    fix null JS error if click trigger not present

--- js/five9-modal.js
+++ js/five9-modal.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const chat_trigger = document.querySelector('.five9-modal__trigger');
 
-    chat_trigger.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        const chat_target = document.getElementById( 'five9-maximize-button' );
-        document.dispatchEvent(new Event("click"));
-        chat_target.dispatchEvent(new Event("click"));
-
-    });
+    if ( chat_trigger ) {
+        chat_trigger.addEventListener('click', function (event) {
+            event.preventDefault();
+            const chat_target = document.getElementById( 'five9-maximize-button' );
+            document.dispatchEvent(new Event("click"));
+            chat_target.dispatchEvent(new Event("click"));
+        });
+    }
 });
