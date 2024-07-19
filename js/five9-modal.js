6d6c267c8c93ce7b55fdf8e06f8623d4ace56872
Nick Davis <inbox@nickdavis.io>
2024-07-19 19:52:45 +0200

    work with multiple triggers on a page

--- js/five9-modal.js
+++ js/five9-modal.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const chat_trigger = document.querySelector('.five9-modal__trigger');
+    const chat_triggers = document.querySelectorAll('.five9-modal__trigger');
 
-    if ( chat_trigger ) {
-        chat_trigger.addEventListener('click', function (event) {
-            event.preventDefault();
-            const chat_target = document.getElementById( 'five9-maximize-button' );
-            document.dispatchEvent(new Event("click"));
-            chat_target.dispatchEvent(new Event("click"));
+    if (chat_triggers.length > 0) {
+        chat_triggers.forEach(trigger => {
+            trigger.addEventListener('click', function (event) {
+                event.preventDefault();
+                const chat_target = document.getElementById('five9-maximize-button');
+                document.dispatchEvent(new Event("click"));
+                if (chat_target) {
+                    chat_target.dispatchEvent(new Event("click"));
+                }
+            });
         });
     }
 });
